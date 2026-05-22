e0a2760f05f94ae1bada4c1953acded8794647b8
kristina-pachi <kristina.pachi.04@bk.ru>
2026-05-22 21:27:12 +0300

    2.2 созданы классы модель и интерфейс для DAO

--- src/main/java/com/movies/task_2/Movie.java
+++ src/main/java/com/movies/task_2/Movie.java
@@ -1,4 +1,45 @@
 package com.movies.task_2;
 
 public class Movie {
+    private Integer id;
+    private String title;
+    private String genre;
+    private Integer releaseYear;
+
+    public Movie() {}
+
+    public Movie(String title, String genre, Integer releaseYear) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public Movie(Integer id, String title, String genre, Integer releaseYear) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public Integer getReleaseYear() { return releaseYear; }
+    public void setReleaseYear(Integer releaseYear) { this.releaseYear = releaseYear; }
+
+    @Override
+    public String toString() {
+        return "Movie{id=" + id +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", releaseYear=" + releaseYear +
+                '}';
+    }
 }
+
--- src/main/java/com/movies/task_2/MovieDAO.java
+++ src/main/java/com/movies/task_2/MovieDAO.java
@@ -1,4 +1,21 @@
 package com.movies.task_2;
 
-public class MovieDAO {
+
+import java.util.*;
+
+public interface MovieDAO {
+
+    void createTable();
+    void dropTable();
+
+    Movie insert(Movie movie);
+    void delete(int id);
+    void updateTitle(int id, String newTitle);
+
+    Optional<Movie> findById(int id);
+    List<Movie> findAll();
+    List<Movie> findByTitle(String part);
+    List<Movie> findByGenre(String genre);
+    List<Movie> findByYear(int year);
 }
+
